1260efedd62bb21cfd07379932e929f66430dc50
Armando Guarino <arm.guarino@gmail.com>
2020-09-08 10:36:09 +0200

    Day 0

---
+++ Day0-Form-Validation-Regular-Expressions/app.js
@@ -0,0 +1,55 @@
+// Form Blur Event Listeners
+document.getElementById("name").addEventListener("blur", validateName);
+document.getElementById("zip").addEventListener("blur", validateZip);
+document.getElementById("email").addEventListener("blur", validateEmail);
+document.getElementById("phone").addEventListener("blur", validatePhone);
+
+function validateName() {
+  const name = document.getElementById("name");
+  // Name must be letters, capital and lowercase, between 2 and 10
+  const re = /^[a-zA-z]{2,10}$/;
+
+  if (!re.test(name.value)) {
+    name.classList.add("is-invalid");
+  } else {
+    name.classList.remove("is-invalid");
+  }
+}
+
+function validateZip() {
+  const zip = document.getElementById("zip");
+  // Zipcode must be 5 digit, sometimes - and 4 more digits at the end
+  const re = /^\d{5}(-\d{4})?$/;
+
+  if (!re.test(zip.value)) {
+    zip.classList.add("is-invalid");
+  } else {
+    zip.classList.remove("is-invalid");
+  }
+}
+
+function validateEmail() {
+  const email = document.getElementById("email");
+
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if (!re.test(email.value)) {
+    email.classList.add("is-invalid");
+  } else {
+    email.classList.remove("is-invalid");
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById("phone");
+
+  // 555-555-555 or 555.555.5555 or (555) 555 5555 or 5555555555
+
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add("is-invalid");
+  } else {
+    phone.classList.remove("is-invalid");
+  }
+}
