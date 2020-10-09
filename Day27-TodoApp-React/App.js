3b856ae32286a3dfe2b529fd77124ed9a3fa20cf
Armando Guarino <arm.guarino@gmail.com>
2020-10-09 15:48:57 +0200

    Day 27

---
+++ Day27-TodoApp-React/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import Form from './components/Form'
+import TodoList from './components/TodoList'
+
+function App() {
+    // States
+    const [inputText, setInputText] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState('all');
+    const [filteredTodos, setFilteredTodos] = useState([]);
+
+    // useEffect
+    useEffect(() => {
+        getLocalTodos();
+    }, []);
+
+    useEffect(() => {
+        filterHandler();
+        saveLocalTodos();
+    }, [todos, status]);
+
+    // Functions
+    const filterHandler = () => {
+        switch(status) {
+            case 'completed':
+                setFilteredTodos(todos.filter(todo => todo.completed === true))
+                break;
+            case 'uncompleted':
+                setFilteredTodos(todos.filter(todo => todo.completed === false))
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        } 
+    }
+
+    // Save to local storage
+    const saveLocalTodos = () => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
+
+    const getLocalTodos = () => {
+        if (localStorage.getItem('todos') === null) {
+            localStorage.setItem('todos', JSON.stringify([]));
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem('todos'))
+            setTodos(todoLocal)
+        }
+    }
+
+    return (
+        <div>
+            <header>
+            <h1>#todo</h1>
+            </header>
+            <Form 
+            setInputText={setInputText} 
+            inputText={inputText}
+            todos={todos} 
+            setTodos={setTodos}
+            setStatus={setStatus}
+            />
+            <TodoList  
+            setTodos={setTodos}
+            todos={todos}
+            filteredTodos={filteredTodos}
+            />
+        </div>
+    )
+}
+
+export default App
